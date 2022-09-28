b4ce118741911b472d46bbbc9b6146f037ecad2e
jiwon709 <jimong8702@naver.com>
2022-09-28 18:44:06 +0900

    config init

---
+++ src/test/java/com/til/autocommit/AutoCommitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.til.autocommit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCommitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
