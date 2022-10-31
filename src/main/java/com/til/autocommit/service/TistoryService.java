23d7c62350448b95e05e335dda4052e01b4cfa9d
jiwon709 <jimong8702@naver.com>
2022-10-31 21:22:34 +0900

    안쓰는 로직 삭제

--- src/main/java/com/til/autocommit/service/TistoryService.java
+++ src/main/java/com/til/autocommit/service/TistoryService.java
@@ -1,14 +1,12 @@
 package com.til.autocommit.service;
 
 import com.til.autocommit.domain.Tistory;
-import com.til.autocommit.dto.TistoryResDto;
 import com.til.autocommit.repository.TistoryRepository;
 import com.til.autocommit.dto.TistoryToGithubReqDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,7 +16,6 @@ public class TistoryService {
     private final TistoryRepository tistoryRepository;
 
     public ResponseEntity<?> searchTistoryList(String name) {
-        List<TistoryResDto> tistoryDto = new ArrayList<>();
         List<Tistory> tistorys = tistoryRepository.findAllByName(name);
 
         List<TistoryToGithubReqDto> collect = tistorys.stream()
