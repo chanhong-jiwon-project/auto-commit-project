bd7c8672d433b4f5cdcd6eb6b43013d2fc54aff9
jiwon709 <jimong8702@naver.com>
2022-11-21 21:17:41 +0900

    게시판 데이터 넣기 controller

--- src/main/java/com/til/autocommit/controller/BoardController.java
+++ src/main/java/com/til/autocommit/controller/BoardController.java
@@ -1,10 +1,14 @@
 package com.til.autocommit.controller;
 
+import com.til.autocommit.dto.BoardReqDto;
 import com.til.autocommit.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,4 +27,13 @@ public class BoardController {
         System.out.println("list controller start");
         return boardService.searchBoard();
     }
+
+    //게시판 데이터 넣기
+    @PostMapping(value = "/board/save")
+    public ResponseEntity<?> saveBoard(@RequestBody BoardReqDto boardReqDto){
+        System.out.println("board write start");
+        return boardService.saveBoard(boardReqDto);
+    }
+
+
 }
