3ee791c64c5c5a589464457b593cd43147f143dc
jiwon709 <jimong8702@naver.com>
2022-11-21 21:17:08 +0900

    게시판 데이터 조회 controller

--- src/main/java/com/til/autocommit/controller/BoardController.java
+++ src/main/java/com/til/autocommit/controller/BoardController.java
@@ -1,13 +1,26 @@
 package com.til.autocommit.controller;
 
+import com.til.autocommit.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService boardService;
     @GetMapping(value = "/board")
     public String board(){
         System.out.println("index 호출성공");
         return "/";
+    }
+
+    //게시판 리스트 전체 가져오기
+    @GetMapping(value = "/board/list")
+    public ResponseEntity<?> boardList(){
+        System.out.println("list controller start");
+        return boardService.searchBoard();
     }
 }
